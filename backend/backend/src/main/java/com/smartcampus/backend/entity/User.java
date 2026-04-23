a8866a9f013b48f56d12ee4adf4840c1e9f393a4
hirushijayasundara <wikumsurindu542@gmail.com>
2026-04-24 00:19:45 +0530

    admin dashboard backend, globalexceptionhandler added

---
+++ backend/backend/src/main/java/com/smartcampus/backend/entity/User.java
@@ -0,0 +1,32 @@
+package com.smartcampus.backend.entity;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private String id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Role is required")
+    private String role; // USER or ADMIN
+}
